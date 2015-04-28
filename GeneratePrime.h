d2a910880fa2d8434ca413d5a1e3d17adbcfc418
MonsieurWilson <syslxy1991@gmail.com>
2015-04-29 01:19:14 +0800

    201504290119

--- GeneratePrime.h
+++ GeneratePrime.h
@@ -3,23 +3,23 @@
 
 class Generate {
 public:
-	void generatePrime(const int &N) {
-		int p[N] = {0};
-		for (int i = 1; i < N; i += 2) {
-			p[i] = 1;
-		}
-		for (int i = 2; i < N; ++i) {
-			for (int j = i * i; j < N; j += i) {
-				p[j] = 0;
-			}
-		}
-		p[2] = 1;
-		for (int i = 0; i < N; ++i) {
-			if (p[i]) {
-				cout<<i<<endl;
-			}
-		}
-	}
+    void generatePrime(const int &N) {
+        int p[N] = {0};
+        for (int i = 1; i < N; i += 2) {
+            p[i] = 1;
+        }
+        for (int i = 2; i < N; ++i) {
+            for (int j = i * i; j < N; j += i) {
+                p[j] = 0;
+            }
+        }
+        p[2] = 1;
+        for (int i = 0; i < N; ++i) {
+            if (p[i]) {
+                cout<<i<<endl;
+            }
+        }
+    }
 };
 
 #endif
--- kmp.cpp
+++ kmp.cpp
@@ -12,15 +12,15 @@ public:
         int ret = 0;
         if (m == 0) return ret;
         // Calculating prefix function for pattern.
-		vector<int> prefix(m, 0);
+        vector<int> prefix(m, 0);
         for (int i = 1; i < m; i++) {
             prefix[i] = prefix[i - 1];
             while (prefix[i] > 0 && pattern[prefix[i]] != pattern[i]) {
                 prefix[i] = prefix[prefix[i] - 1];
-			}
+            }
             if (pattern[prefix[i]] == pattern[i]) {
                 prefix[i]++;
-			}
+            }
         }
         for (int idx = 0, p = 0; idx < n; idx++) {
             // When failed to match the index p of pattern, 
@@ -32,7 +32,7 @@ public:
             if (pattern[p] == source[idx]) {
                 p++;
             }
-			// Having reached the tail of the pattern.
+            // Having reached the tail of the pattern.
             if (p == m) {
                 ++ret;
                 // Jumping to the next matching index.
