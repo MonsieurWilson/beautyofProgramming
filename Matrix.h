724a1fd7e2180ccb03d49229fb46b22a69e5acd5
MonsieurWilson <syslxy1991@gmail.com>
2015-09-17 00:15:38 +0800

    201509170015
    Signed-off-by: MonsieurWilson <syslxy1991@gmail.com>

---
+++ Matrix.h
@@ -0,0 +1,125 @@
+#ifndef MATRIX
+#define MATRIX
+
+#include <vector>
+
+class Matrix {
+private:
+    vector<vector<int> > mat;
+    int row, col;
+public:
+    Matrix(const int &r, const int &c): row(r), col(c) {
+        mat = vector<vector<int> >(row, vector<int>(col, 0));
+    }
+    Matrix(const Matrix &m) {
+        this->row = m.size()[0];
+        this->col = m.size()[1];
+        mat = vector<vector<int> >(row, vector<int>(col, 0));
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                setEle(r, c, m.getEle(r, c));
+            }
+        }
+    }
+    int getEle(const int &r, const int &c) const {
+        return mat[r][c];
+    }
+    void setEle(const int &r, const int &c, const int &val) {
+        mat[r][c] = val;
+    }
+    vector<int> size() const {
+        return vector<int>{row, col};
+    }
+    void resize(const int &r, const int &c) {
+        mat = vector<vector<int> >(r, vector<int>(c, 0));
+    }
+    Matrix &operator = (const Matrix &m) {
+        this->row = m.size()[0];
+        this->col = m.size()[1];
+        resize(row, col);
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                setEle(r, c, m.getEle(r, c));
+            }
+        }
+        // !
+        return *this;
+    }
+    Matrix operator * (const Matrix &mat) {
+        vector<int> size = mat.size();
+        Matrix ret(this->row, size[1]);
+        for (int r1 = 0; r1 < this->row; ++r1) {
+            for (int c2 = 0; c2 < size[1]; ++c2) {
+                int ele = 0;
+                for (int i = 0; i < this->col; ++i) {
+                    ele += getEle(r1, i) * mat.getEle(i, c2);
+                }
+                ret.setEle(r1, c2, ele);
+            }
+        }
+        return ret;
+    }
+    Matrix operator + (const Matrix &mat) {
+        Matrix ret(this->row, this->col);
+        for (int r = 0; r < this->row; ++r) {
+            for (int c = 0; c < this->col; ++c) {
+                ret.setEle(r, c, getEle(r, c) + mat.getEle(r, c));
+            }
+        }
+        return ret;
+    }
+    Matrix operator - (const Matrix &mat) {
+        Matrix ret(this->row, this->col);
+        for (int r = 0; r < this->row; ++r) {
+            for (int c = 0; c < this->col; ++c) {
+                ret.setEle(r, c, getEle(r, c) - mat.getEle(r, c));
+            }
+        }
+        return ret;
+    }
+
+};
+
+
+ostream &operator << (ostream &o, const Matrix &mat) {
+    o << "[" << endl;
+    vector<int> size = mat.size();
+    for (int r = 0; r < size[0]; ++r) {
+        o << "  ";
+        for (int c = 0; c < size[1]; ++ c) {
+            o << mat.getEle(r, c) << " ";
+        }
+        cout << endl;
+    }
+    o << "]";
+    return o;
+}
+
+void crtMat(Matrix &mat, const int &r, const int &c) {
+    cout << "Input the matrix's elements:" << endl;
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            int tmp;
+            cin >> tmp;
+            mat.setEle(i, j, tmp);
+        }
+    }
+}
+
+Matrix matPow(const Matrix &mat, int n) {
+    int row = mat.size()[0];
+    Matrix ret(row, row);
+    for (int i = 0; i < row; ++i) {
+        ret.setEle(i, i, 1);
+    }
+    Matrix tmp(mat);
+    for (; n; n >>= 1) {
+        if (n & 1) {
+            ret = ret * tmp;
+        }
+        tmp = tmp * tmp;
+    }
+    return ret;
+}
+
+#endif
---
+++ fibonacci.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#include "Matrix.h"
+
+class Fibonacci {
+public:
+    int genFibonacci(const int &n) {
+        // F(n) = F(n - 1) + F(n - 2)
+        // O(N) method
+        int tmp1, tmp2, ret;
+        tmp1 = 0;
+        tmp2 = 1;
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        for (int i = 2; i <= n; ++i) {
+            ret = tmp1 + tmp2;
+            tmp1 = tmp2;
+            tmp2 = ret;
+        }
+        return ret;
+    }
+
+    int genFibonacciII(int n) {
+        // [F(n) F(n - 1)] = [F(n - 1) F(n - 2)] * A
+        // A = [1, 1; 1, 0]
+        // F(n) = F(1) * An[0][0] + F(0) * An[1][0]
+        // O(long(N)) method
+        int F0 = 0, F1 = 1;
+        if (n == 0) {
+            return F0;
+        }
+        else if (n == 1) {
+            return F1;
+        }
+        Matrix A(2, 2);
+        A.setEle(0, 0, 1);
+        A.setEle(0, 1, 1);
+        A.setEle(1, 0, 1);
+        Matrix An = matPow(A, n - 1);
+        return F1 * An.getEle(0, 0) + F0 * An.getEle(1, 0);
+    }
+
+};
+
+int main() {
+    Fibonacci f;
+    cout << "Input the value of number n:" << endl;
+    int n;
+    while (cin >> n) {
+        printf("The %dth fibonacci number is: %d\n", n, f.genFibonacciII(n));
+    }
+
+    return 0;
+}
